c6d5a24cc2c11d68875f7146dc8c0d5fd2b04ee2
Vivek Sutar <viveksutar26@gmail.com>
2023-01-07 21:25:51 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  title = 'This is data-binding';
+
+  string = "String Interpollation Example"
 
   no: number = 0;
 
